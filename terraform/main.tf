dd1b9b026836b92f74dae6480c352927e7f41576
ssaifee <ssaifee@covermymeds.com>
2023-06-04 19:52:33 -0400

    create aks cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,28 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.59.0"
-    }
-  }
+resource "azurerm_resource_group" "main" {
+  name     = "kubecon-2023"
+  location = var.location
 }
 
-provider "azurerm" {
-  features {}
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "kyverno"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  default_node_pool {
+    name                = "default"
+    node_count          = 1
+    vm_size             = "Standard_D2_v2"
+    enable_auto_scaling = true
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
 }
